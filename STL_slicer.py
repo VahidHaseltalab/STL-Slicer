2cb859d430ded572d63a76b845ab9ed638f4f7eb
Haseltalab <53440292+Haseltalab@users.noreply.github.com>
2020-02-21 15:26:06 +0300

    Update STL_slicer.py

--- STL_slicer.py
+++ STL_slicer.py
@@ -10,6 +10,7 @@ import shapely.geometry as sg
 
 ### Slicing an STL file format. It requires STL address or vertices of a stl file,
 ### the height of slice plane and direction of the slicing (xy,xz,yz)
+### Slicing an STL file format. It requires STL address, and the height of slice plane
 class Slice:
     def vertices(self,a):
         # importing ths stl file
@@ -29,6 +30,7 @@ class Slice:
     def __init__(self,addr = 'bunny.stl',direction = 'xy'):
         if type(addr) == str:
             self.vrt,self.zvalues = self.vertices(addr)
+            self.direction = direction
         else:
             self.direction = direction
             self.vrt = addr
@@ -40,24 +42,31 @@ class Slice:
                 self.zvalues = addr.reshape([len(addr)*3,3])[:,0]
      ### finds intersection coordinates between a point and a line 
     def eqn1(self,p1,p2,z):
+        if p1[2]==p2[2]: return tuple([p1[0],p1[1]])
         ### the ratio that can apply to distance of x and y 
         t = (z-p1[2]) / (p2[2]-p1[2]) 
-        return [p1[0] + (p2[0]-p1[0])*t , p1[1] + (p2[1]-p1[1])*t]
+        return (p1[0] + (p2[0]-p1[0])*t , p1[1] + (p2[1]-p1[1])*t)
     def eqn2(self,p1,p2,z):
+        if p1[1]==p2[1]: return tuple([p1[0],p1[2]])
         ### the ratio that can apply to distance of x and y 
         t = (z-p1[1]) / (p2[1]-p1[1]) 
-        return [p1[0] + (p2[0]-p1[0])*t , p1[2] + (p2[2]-p1[2])*t] 
+        return (p1[0] + (p2[0]-p1[0])*t , p1[2] + (p2[2]-p1[2])*t)
     def eqn3(self,p1,p2,z):
+        if p1[0]==p2[0]: return tuple([p1[1],p1[2]])
         ### the ratio that can apply to distance of x and y 
         t = (z-p1[0]) / (p2[0]-p1[0]) 
-        return [p1[1] + (p2[1]-p1[1])*t , p1[2] + (p2[2]-p1[2])*t] 
+        return (p1[1] + (p2[1]-p1[1])*t , p1[2] + (p2[2]-p1[2])*t)
     ### checks whether the z plane is crossing through the line
     def checkline(self,zl,z):
-        if z < max(zl) and z > min(zl):
+        if z <= np.max(zl) and z >= np.min(zl):
             return True
         else: return False
     ### finds intersection coordinates between a plane and a triangular facet        
     def trintersct(self,l,z):
+        l = np.array(l)
+        if self.direction == 'xy' and (l[:,2] == z).all(): return []
+        elif self.direction == 'xz' and (l[:,1] == z).all(): return []
+        elif self.direction == 'yz' and (l[:,0] == z).all(): return []
         inlst = []
         for i in range(3):
             pt1 = l[i];pt2 = l[i-1]
@@ -76,6 +85,11 @@ class Slice:
                 if self.checkline(zl,z):
                     p = self.eqn3(pt1,pt2,z)
                     inlst.append(p)
+        if len(inlst) == 3:
+            if np.array_equal(np.round(inlst[0],4),np.round(inlst[1],4)): del(inlst[1])
+            else: del(inlst[2])
+        if inlst:
+            if np.array_equal(np.round(inlst[0],4),np.round(inlst[1],4)): return []
         return inlst
     #The function clear will remove empty lists from S
     def clear(self,A):
@@ -88,82 +102,68 @@ class Slice:
                 W[k]=A[i]
                 k+=1
         return W
-    
-    def optlst(self,L2,p,r):
-        idx1 = np.where((L2[:,0]>p[0]-r)&(L2[:,0]<p[0]+r))[0]
-        L3 = L2[idx1]
-        L4 = L3[L3[:,1].argsort()]
-        idx2 = np.where((L4[:,1]>p[1]-r)&(L4[:,1]<p[1]+r))[0]
-        L5 = L4[idx2]
-        return L5
-    ### Ordering the points in a correct sequence
-    def order1(self,L,r):
-        l = [[] for _ in range(5)]
-        i = 0
-        p = L[0]
-        del(L[0])
-        T1 = spatial.cKDTree(L)
-        l[i].append(p)
-        while L:
-            ds, idx = T1.query(p)
-            pp = L[idx]
-            if ds > r:
-                i += 1
-            l[i].append(pp)
-            del(L[idx])
-            if not L:
-                break
-            T1 = spatial.cKDTree(L)
-            p = pp
-        l = self.clear(l)
-        for a in l:
-            a.append(a[0])
-        return l
-    def order2(self,L):
-        L = np.round(L,5); lst = []
-        lst.extend(L[:1].tolist()); a = np.array(lst[-1])
-        L = np.delete(L,0,axis=0)
-        while L.any():
-            vec = L-a; nn = np.linalg.norm(vec,axis=1)
-            idx1 = nn.argsort()[:1][0]; lst.append(L[idx1].tolist())
-            L = np.delete(L,idx1,axis=0); a = np.array(lst[-1])
-        lst.append(lst[0])
-        return lst
-    ### order the sequence of points to form polygon
-    def order3(self,L):
-        convex = sg.MultiPoint(L).convex_hull.buffer(3).buffer(-3)
-        line = sg.LineString(L)
-        k1 = [L.copy() for b in range(3)]
-        try: ff = sg.Polygon(L); pl0 = L
-        except: pl0 = []
-        if convex.length*2 < line.length:
-            pl1 = list(convex.exterior.coords)
-        else: pl1 = []
+    def totuple(self,a): ## convert an array to nested tuple
         try:
-            pl2 = self.order2(k1[1])
-            if len(pl2) == 1 and len(pl2[0] > 2): pl2 = pl2[0] 
-        except: pl2 = []
-        area = [sg.Polygon(a).area for a in [pl0,pl1,pl2]]
-        idx = area.index(max(area))
-        pl = [pl0,pl1,pl2][idx]
-        if len(pl) > 2:
-            p = sg.Polygon(pl).buffer(1).buffer(-1)
-            if p.area != 0:
-                return list(p.exterior.coords)
-            else: return list(convex.exterior.coords)
-        else: return []
+            return tuple(self.totuple(i) for i in a)
+        except TypeError:
+            return a
+    def order(self,pnts,prec = 5):
+        if not pnts: return []
+        pnts = np.round(pnts,prec)
+        pnts = self.totuple(pnts)
+        wires = [];Q1 = set()
+        for pn in pnts:
+            #pn = simplify(pn,0.01)
+            if pn not in Q1:
+                Q = list(pn)
+                Q1.add(pn)
+                a1 = Q[0]; a2 = Q[1]; a3 = Q[-2]; a4 = Q[-1]
+                c = 0
+                while c < 1:
+                    run = False
+                    for b in pnts:
+                        #b = simplify(b,0.01)
+                        if b not in Q1:
+                            b1 = b[0]; b2 = b[-1]
+                            if a4 == b1:
+                                run = True
+                                Q = Q + list(b)
+                                Q1.add(b)
+                                a3 = b1; a4 = b2
+                            elif a1 == b2:
+                                run = True
+                                Q = list(b) + Q
+                                Q1.add(b)
+                                a1 = b1; a2 = b2
+                            elif a4 == b2:
+                                run = True
+                                Q = Q + list(b)[::-1]
+                                Q1.add(b)
+                                a3 = b2; a4 = b1
+                            elif a1 == b1:
+                                run = True
+                                Q = list(b)[::-1] + Q
+                                Q1.add(b)
+                                a1 = b2; a2 = b1
+                    if not run: c+=1
+                wires.append(Q)
+        if len(wires) == 1:
+            return wires[0]
+        if prec == 0: return wires[0]
+        else: return self.order(wires,prec-1)
     def cross_section(self,z):
         z1 = np.array(self.zvalues).reshape((int(len(self.zvalues)/3),3))
-        idx = np.where((z1>z-4)&(z1<z+4))[0]
+        idx = np.where((z1>z-5)&(z1<z+5))[0]
         fnum = set()
         L = []
-        for a in idx:
+        for a in list(set(idx)):
             if a not in fnum:
                 fnum.add(a)
-                L.extend(self.trintersct(self.vrt[a],z))
-        pl = self.order3(L)
-        if list(pl):
-            return pl
-        else: 
-            print('No intersection!')
-            return []
+                cr = self.trintersct(self.vrt[a],z)
+                if cr: L.append(cr)
+        L1 = np.round(L,3)
+        lines = list(so.polygonize(L1.tolist()))
+        if lines:
+            return list(lines[0].exterior.coords)
+        else:
+            return list(self.order(L))
